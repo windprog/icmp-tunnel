2a7396ca5afebb906ad494422ffd0694496aef90
Windpro <windprog@gmail.com>
2015-06-19 11:59:01 +0800

    muti send

--- server.py
+++ server.py
@@ -87,6 +87,69 @@ class AESCipher:
             ret = None
         return ret
 
+class TunnelPacket(ICMPPacket):
+    def __init__(self, buf=None):
+        self.tunnel_id = None
+        super(TunnelPacket, self).__init__(buf)
+
+    def loads(self, buf):
+        super(ICMPPacket, self).loads(buf)
+        # self.tunnel_id为原来data字段的头两个字节
+        self.type, self.code, self.chksum, self.id, self.seqno, self.tunnel_id = struct.unpack("!BBHHHL", buf[20:32])
+        self.data = buf[32:]
+
+    @classmethod
+    def create(cls, _type, code, _id, seqno, tunnel_id, data):
+        pk = cls()
+        pk.type, pk.code, pk.id, pk.seqno, pk.tunnel_id, pk.data = _type, code, _id, seqno, tunnel_id, data
+        return pk
+
+    def dumps(self):
+        packfmt = "!BBHHHL%ss" % (len(self.data))
+        args = [self.type, self.code, 0, self.id, self.seqno, self.tunnel_id, self.data]
+        args[2] = ICMPPacket.checksum(struct.pack(packfmt, *args))
+        return struct.pack(packfmt, *args)
+
+class PacketControl(object):
+    def __init__(self, tunnel):
+        self.tunnel = tunnel
+        assert isinstance(self.tunnel, Tunnel)
+        self.builder_class = ICMPPacket
+        self.now_send_id = long(0)
+        self.recv_ids = []
+        self.cipher = AESCipher(PASSWORD)
+
+    def get_send_id(self):
+        self.now_send_id += 1
+        if self.now_send_id >= sys.maxint:
+            self.now_send_id = long(1)
+        return self.now_send_id
+
+    def send(self, buf):
+        buf = self.cipher.encrypt(buf)
+        if buf is None:
+            return
+        ipk = TunnelPacket.create(
+            self.tunnel.icmp_type, 0, self.tunnel.now_icmp_identity, 0x4147, self.get_send_id(), buf).dumps()
+        for _ in range(2):
+            self.tunnel.icmpfd.sendto(ipk, (self.tunnel.DesIp, 22))
+
+    def recv(self):
+        buf = self.tunnel.icmpfd.recv(2048)
+        packet = TunnelPacket(buf)
+        packet.data = self.cipher.decrypt(packet.data)
+        if packet.seqno != 0x4147:  # True packet
+            return None
+        if not packet.data:
+            return None
+        if packet.tunnel_id not in self.recv_ids:
+            if len(self.recv_ids) > 100000:
+                self.recv_ids.pop(0)
+            self.recv_ids.append(packet.tunnel_id)
+            return packet
+        else:
+            return None
+
 
 class Tunnel(object):
     def __init__(self, is_server, des_ip=None):
@@ -100,9 +163,9 @@ class Tunnel(object):
         self.tunfd = self.get_tun()
         self.epoll.register(self.tunfd.fileno(), select.EPOLLIN)
 
-        self.cipher = AESCipher(PASSWORD)
+        self.now_icmp_identity = 0xffff
 
-        self.NowIdentity = 0xffff
+        self.control = PacketControl(self)
         if is_server:
             self.icmp_type = SERVER_ICMP_TYPE
         else:
@@ -132,24 +195,17 @@ class Tunnel(object):
             for fileno, event in events:
                 if fileno == self.tunfd.fileno():
                     buf = self.tunfd.read(2048)
-                    buf = self.cipher.encrypt(buf)
-                    if buf is None:
-                        continue
-                    ipk = ICMPPacket.create(self.icmp_type, 0, self.NowIdentity, 0x4147, buf).dumps()
-                    self.icmpfd.sendto(ipk, (self.DesIp, 22))
+                    self.control.send(buf)
                 elif fileno == self.icmpfd.fileno():
-                    buf = self.icmpfd.recv(2048)
-                    packet = ICMPPacket(buf)
+                    packet = self.control.recv()
+                    if not packet:
+                        continue
                     data = packet.data
-                    if packet.seqno == 0x4147:  # True packet
-                        self.NowIdentity = packet.id
-                        if self.is_server:
-                            des_ip = socket.inet_ntoa(packet.src)
-                            self.DesIp = des_ip
-                        data = self.cipher.decrypt(data)
-                        if not data:
-                            continue
-                        self.tunfd.write(data)
+                    self.now_icmp_identity = packet.id
+                    if self.is_server:
+                        des_ip = socket.inet_ntoa(packet.src)
+                        self.DesIp = des_ip
+                    self.tunfd.write(data)
 
 
 if __name__ == '__main__':
